6bf756a9415c05a614a6a705de9fed651f1469d2
Tymoteusz Machowski <tmachows@gmail.com>
2018-05-13 16:38:02 +0200

    add keychain & nfd ping

--- src/main/java/pl/edu/agh/sm/ndnserver/Server.java
+++ src/main/java/pl/edu/agh/sm/ndnserver/Server.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.sm.ndnserver;
 
 import net.named_data.jndn.*;
+import net.named_data.jndn.encoding.EncodingException;
+import net.named_data.jndn.security.KeyChain;
 import net.named_data.jndn.security.SecurityException;
+import net.named_data.jndn.security.identity.IdentityManager;
+import net.named_data.jndn.security.identity.MemoryIdentityStorage;
+import net.named_data.jndn.security.identity.MemoryPrivateKeyStorage;
 import net.named_data.jndn.util.Blob;
 
 import java.io.IOException;
@@ -10,6 +15,18 @@ public class Server {
 
     public static void main(String[] args) {
         Face face = new Face("localhost");
+        try {
+            KeyChain keyChain = buildKeyChain(face);
+            face.setCommandSigningInfo(keyChain, keyChain.getDefaultCertificateName());
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            pingNdnDaemon(face);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Name prefix = new Name("/pl/edu/agh/sm/demo");
         OnInterestCallback onInterestCallback = createOnInterestCallback();
@@ -19,6 +36,35 @@ public class Server {
         } catch (IOException | SecurityException e) {
             e.printStackTrace();
         }
+
+        while (true) {
+            try {
+                face.processEvents();
+                Thread.sleep(100);
+            } catch (IOException | EncodingException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static KeyChain buildKeyChain(Face face) throws SecurityException {
+        MemoryIdentityStorage identityStorage = new MemoryIdentityStorage();
+        MemoryPrivateKeyStorage privateKeyStorage = new MemoryPrivateKeyStorage();
+        IdentityManager identityManager = new IdentityManager(identityStorage, privateKeyStorage);
+        KeyChain keyChain = new KeyChain(identityManager);
+        try {
+            keyChain.getDefaultCertificateName();
+        } catch (SecurityException e) {
+            keyChain.createIdentityAndCertificate(new Name("/test/identity"));
+            keyChain.getIdentityManager().setDefaultIdentity(new Name("/test/identity"));
+        }
+        return keyChain;
+    }
+
+    private static void pingNdnDaemon(Face face) throws IOException {
+        face.expressInterest(new Name("/localhost/nfd/status/general"),
+                (interest, data) -> System.out.println("Ping response received: " + data.getContent().toString()),
+                interest -> System.out.println("Error: ping timeout"));
     }
 
     private static OnInterestCallback createOnInterestCallback() {
